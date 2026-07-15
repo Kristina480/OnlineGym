ca7630fdd3a4caf038af997d1d70db20a3b85100
Neda Tesic <nedatesic19@gmail.com>
2026-07-15 21:38:35 +0200

    Add MapFromReader and GetById implementation

--- src/OnlineGym.Application/Database/Repository/CollaborationRequestRepository.cs
+++ src/OnlineGym.Application/Database/Repository/CollaborationRequestRepository.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using OnlineGym.Application.Domain;
+using OnlineGym.Application.Domain.Enums;
 
 namespace OnlineGym.Application.Database.Repositories;
 
@@ -23,5 +24,39 @@ VALUES
          // check if row was added - successful insert
          int rowsAffected = command.ExecuteNonQuery();
          return rowsAffected > 0;
+     }
+     public CollaborationRequest? GetById(long id)
+     {
+         using IDbConnection connection = PostgresConnection.CreateConnection();
+         IDbCommand command = connection.CreateCommand();
+
+         command.CommandText = @"
+        SELECT *
+        FROM collaboration_requests
+        WHERE request_id = @request_id;";
+
+         DataBaseHelper.AddParameter(command, "@request_id", id);
+
+         using IDataReader reader = command.ExecuteReader();
+
+         if (reader.Read())
+         {
+             return MapFromReader(reader);
+         }
+
+         return null;
+     }
+     
+     private CollaborationRequest MapFromReader(IDataReader reader)
+     {
+         return new CollaborationRequest(
+             reader.GetInt64(reader.GetOrdinal("request_id")),
+             reader.GetInt64(reader.GetOrdinal("client_id")),
+             reader.GetInt64(reader.GetOrdinal("trainer_id")),
+             reader.GetDateTime(reader.GetOrdinal("request_date")),
+             Enum.Parse<RequestStatus>(
+                 reader.GetString(reader.GetOrdinal("status")),
+                 true)
+         );
      }
  }
