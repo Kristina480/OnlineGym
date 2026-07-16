962a635bb4d4104906eca1596ea8ebfb2e4a811d
Neda Tesic <nedatesic19@gmail.com>
2026-07-16 02:10:45 +0200

    Add SendRequest function

--- src/OnlineGym.Application/Services/CollaborationRequestService.cs
+++ src/OnlineGym.Application/Services/CollaborationRequestService.cs
@@ -1,3 +1,5 @@
+using OnlineGym.Application.Domain;
+using OnlineGym.Application.Domain.Enums;
 using OnlineGym.Application.Interfaces.Repositories;
 
 namespace OnlineGym.Application.Services;
@@ -22,5 +24,34 @@ public class CollaborationRequestService
         _clientRepository = clientRepository;
         _trainerRepository = trainerRepository;
         _pricingPackageRepository = pricingPackageRepository;
+    }
+
+    public bool SendRequest(long clientId, long trainerId)
+    {
+        if (_clientRepository.GetById(clientId) is null)
+            throw new InvalidOperationException(
+                $"Client with ID {clientId} does not exist.");
+
+        if (_trainerRepository.GetById(trainerId) is null)
+            throw new InvalidOperationException(
+                $"Trainer with ID {trainerId} does not exist.");
+
+        if (_requestRepository.HasPendingRequest(clientId, trainerId))
+            throw new InvalidOperationException(
+                "A pending request for this trainer already exists.");
+
+        if (_collaborationRepository.HasActiveCollaboration(clientId, trainerId))
+            throw new InvalidOperationException(
+                "YOU already has an active collaboration with this trainer.");
+
+        CollaborationRequest request = new CollaborationRequest(
+            0,
+            clientId,
+            trainerId,
+            DateTime.Today,
+            RequestStatus.Pending
+        );
+
+        return _requestRepository.Insert(request);
     }
 }
