0a0a5271e8783c90bbd2028604f6b98df3cf2cc8
Maja <mmatkovic207@gmail.com>
2026-07-18 08:38:19 +0200

    Add Equipment domain class

---
+++ src/OnlineGym.Application/Domain/Equipment.cs
@@ -0,0 +1,18 @@
+namespace OnlineGym.Application.Domain;
+
+public class Equipment
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+
+    public Equipment(long id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Name}";
+    }
+}
